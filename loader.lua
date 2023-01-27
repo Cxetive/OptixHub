75b0351e410ee37685c0bd97e54868e5064025bc
Cxetive <54766299+Cxetive@users.noreply.github.com>
2023-01-27 19:23:15 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,4 +4,6 @@ elseif game.PlaceId == 11542692507 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Cxetive/OptixHub/main/ass.lua"))()
 elseif game.PlaceId == 11767933308 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Cxetive/OptixHub/main/sng.lua"))()
+elseif game.PlaceId == 11334163219 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Cxetive/OptixHub/main/aps.lua"))()  
 end
